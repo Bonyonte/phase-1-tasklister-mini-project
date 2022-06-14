4d60d8854228b16850c8c5b03ca2fc5850dbdd21
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-14 17:04:39 +0300

    competed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (event) => {
+    toDoList (document.querySelector('#new-task-description').value)
+    event.preventDefault()
+  })
+  
+  function toDoList(toDo){
+    let list = document.createElement("li")
+    list.innerText = `${toDo}`
+  document.querySelector('#tasks').appendChild(list)
+  }
 });
+
+
